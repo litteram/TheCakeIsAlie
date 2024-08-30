2a57820e16b5c33cb1355f2332ff25716d73c472
Lorenzo Giuliani <lorenzo@frenzart.com>
2024-08-30 02:16:16 +0200

    init defaults

--- addon.lua
+++ addon.lua
@@ -1,8 +1,76 @@
 local addonName, addon = ...
-
-if addon.db == nil then
-    addon.db = _G.TheCakeIsAlieDB
+addon = addon or {}
+if _G.TheCakeIsAlieDB == nil then
+    _G.TheCakeIsAlieDB = {
+        cinematics_esc = true,
+        cinematics_destroy = false,
+        block_rightclick = false,
+        map_pin = true,
+    }
 end
+addon.db = addon.db or _G.TheCakeIsAlieDB
+
+addon:RegisterSlash("/alie", function()
+    addon:Dump(TheCakeIsAlieDB)
+end)
+
+function trimMultiline(str)
+    local out = ""
+    local indent = 0
+    for nline, line in str:gmatch("[^\r\n]+") do
+        local ind = 0
+
+        for i = 1, #line do
+            local c = line:sub(i,i)
+            ind = ind + 1
+            if c ~= " " and c ~= "\t" then
+                line = line:sub(i, #line)
+            end
+        end
+        if nline ~= 0 and ind < indent then
+            indent = ind
+        end
+    end
+
+    return str
+end
+
+addon:RegisterSettings("TheCakeIsAlieDB", {
+    {
+        key = "cinematics_esc",
+        type = "toggle",
+        title = "Quick cancel cinematics",
+        tooltip = "Quickly cancel cinematics with a single keypress (esc)",
+        default = false,
+        new = false,
+    },
+    {
+        key = "cinematics_destroy",
+        type = "toggle",
+        title = "Automatically cancel all cinematics",
+        tooltip = "Automatically cancel all cinematics without any confirmations. Recommended to be off.",
+        default = false,
+        new = false,
+        
+    },
+    {
+        key = "block_rightclick",
+        type = "toggle",
+        title = "block right click",
+        tooltip = "Block right click in combat",
+        default = false,
+        new = false,
+    },
+    {
+        key = "map_pin",
+        type = "toggle",
+        title = "Map pin tweaks",
+        tooltip = "Enhance map pin:\n- infinite tracking\n- time to arrive",
+        default = true,
+        new = false,
+    },
+})
+
 
 local bindings = {}
 function addon:BindButton(name, key, template)
--- modules/automation/cinematics.lua
+++ modules/automation/cinematics.lua
@@ -1,43 +1,43 @@
 local _, addon = ...
 
-function addon:CINEMATIC_START()
-    if not addon.db.cinematics then
-        return
-    end
+if addon.db.cinematics_destroy then
+    function addon:CINEMATIC_START()
+        if not IsModifierKeyDown() then
+            CinematicFrame_CancelCinematic()
+        end
 
-    if not IsModifierKeyDown() then
-        CinematicFrame_CancelCinematic()
-    end
-
-    local PlayMovie_hook = MovieFrame_PlayMovie
-    MovieFrame_PlayMovie = function(...)
-        if IsModifierKeyDown() or not addon.db.cinematics then
-            PlayMovie_hook(...)
-        else
-            GameMovieFinished()
+        local PlayMovie_hook = MovieFrame_PlayMovie
+        MovieFrame_PlayMovie = function(...)
+            if IsModifierKeyDown() or not addon.db.cinematics then
+                PlayMovie_hook(...)
+            else
+                GameMovieFinished()
+            end
         end
     end
 end
 
 
-CinematicFrame:HookScript("OnKeyDown", function(self, key)
-    if key == "ESCAPE" then
-        if CinematicFrame:IsShown() and CinematicFrame.closeDialog and CinematicFrameCloseDialogConfirmButton then
-            CinematicFrameCloseDialog:Hide()
+if addon.db.cinematics_esc then
+    CinematicFrame:HookScript("OnKeyDown", function(self, key)
+        if key == "ESCAPE" then
+            if CinematicFrame:IsShown() and CinematicFrame.closeDialog and CinematicFrameCloseDialogConfirmButton then
+                CinematicFrameCloseDialog:Hide()
+            end
         end
-    end
-end)
-CinematicFrame:HookScript("OnKeyUp", function(self, key)
-    if key == "SPACE" or key == "ESCAPE" or key == "ENTER" then
-        if CinematicFrame:IsShown() and CinematicFrame.closeDialog and CinematicFrameCloseDialogConfirmButton then
-            CinematicFrameCloseDialogConfirmButton:Click()
+    end)
+    CinematicFrame:HookScript("OnKeyUp", function(self, key)
+        if key == "SPACE" or key == "ESCAPE" or key == "ENTER" then
+            if CinematicFrame:IsShown() and CinematicFrame.closeDialog and CinematicFrameCloseDialogConfirmButton then
+                CinematicFrameCloseDialogConfirmButton:Click()
+            end
         end
-    end
-end)
-MovieFrame:HookScript("OnKeyUp", function(self, key)
-    if key == "SPACE" or key == "ESCAPE" or key == "ENTER" then
-        if MovieFrame:IsShown() and MovieFrame.CloseDialog and MovieFrame.CloseDialog.ConfirmButton then
-            MovieFrame.CloseDialog.ConfirmButton:Click()
+    end)
+    MovieFrame:HookScript("OnKeyUp", function(self, key)
+        if key == "SPACE" or key == "ESCAPE" or key == "ENTER" then
+            if MovieFrame:IsShown() and MovieFrame.CloseDialog and MovieFrame.CloseDialog.ConfirmButton then
+                MovieFrame.CloseDialog.ConfirmButton:Click()
+            end
         end
-    end
-end)
+    end)
+end
