b1e25c1576c37e920454a0d6c5f6dd32d7ec1f2f
Lorenzo Giuliani <lorenzo@frenzart.com>
2024-11-21 09:09:03 +0100

    clickcasting is so hidden ... this gives it an icon

--- modules/widgets/clickcasting_button.lua
+++ modules/widgets/clickcasting_button.lua
@@ -3,10 +3,17 @@ A, addon = ...
 addon:RegisterEvent('ADDON_LOADED', function(self, name)
     if name ~= A then return end
     _G.AddonCompartmentFrame:RegisterAddon({
-        text = "Click Bindings",
+        text = "Click Binding",
         icon = "Interface\\Icons\\TEMP.blp",
         notCheckable = true,
         func = _G.ToggleClickBindingFrame,
+    })
+
+    LibStub("LibDataBroker-1.1"):NewDataObject("Bunnies!", {
+        type = "data source",
+        text = "Click Binding",
+        icon = "Interface\\Icons\\INV_Chest_Cloth_17",
+        OnClick = function() _G.ToggleClickBindingFrame() end,
     })
 end)
 
